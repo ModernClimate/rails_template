c7a20a0bdd302cbde77bb03214c69c097c89367c
Jason Bucki <jasonbucki@gmail.com>
2015-09-04 00:25:39 -0500

    Replace custom Postgres Ansible config with Ansible Galaxy role

--- setup.sh
+++ setup.sh
@@ -6,21 +6,40 @@ if [ -z "$1" ]
     exit 1
 fi
 
-export APP_NAME=$1
-export APP_DIR=../$APP_NAME
-echo "App name: $APP_NAME"
+app_name=$1
+parent_dir="$(dirname "$dir")"
 
-echo "Creating project directory"
-mkdir $APP_DIR
-cp -R . $APP_DIR
+export app_name
+export app_dir=$(dirname $(pwd))/$app_name
+
+echo "\n\n#################################################################################"
+echo "Initializing app:           $app_name"
+
+echo "Creating project directory: $app_dir"
+mkdir $app_dir
+cp -R . $app_dir
 
 echo "Cleaning up files"
-rm -rf $APP_DIR/.git
-rm -rf $APP_DIR/setup.sh
+rm -rf $app_dir/.git
+rm -rf $app_dir/setup.sh
 
-echo "Setup complete.\n\nThis would be a good time to navigate to $APP_DIR and tweak the Ansible files to fit your app's needs.\nWhen you're done, run 'vagrant up development --provider virtualbox --provision' in $APP_DIR."
-echo "Creating and provisioning VM"
-cd $APP_DIR && vagrant up development --provider virtualbox --provision
+echo "Setup complete"
+echo "#################################################################################\n"
+
+echo "\n#################################################################################"
+
+echo "Next steps:\n"
+
+echo "1. Customize Ansible config for app"
+echo "   vim $app_dir/provisioning\n\n"
+
+echo "2. Create and provision your VM"
+echo "   cd $app_dir && \\"
+echo "   vagrant up development --provider virtualbox --provision\n\n"
+
+echo "When provisioning is complete, your Rails app should be running on the guest/VM at http://localhost:3000/"
+
+echo "#################################################################################\n\n"
 
 exit 0
 
